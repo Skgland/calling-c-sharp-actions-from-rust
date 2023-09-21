34e76678294e606cb8cf6ebb4e4226cfa7166ea2
Bennet Bleßmann <bb-github@t-online.de>
2023-09-21 21:30:19 +0200

    improve the odds

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -2,7 +2,7 @@ mod c_sharp;
 
 pub fn normal_rust_fn<S: Fn(), F: Fn(i32)>(success: S, failure: F) {
     let value = rand::random();
-    if value % 100 == 0 {
+    if value % 2 == 0 {
         success();
     } else {
         failure(value);
