c701eb71d8aacc7a6b124d9e923cbc73041f33be
Bennet Bleßmann <bb-github@t-online.de>
2024-06-13 23:25:21 +0200

    use generics to reduce dupplication

--- c-sharp/CSharpBin/Program.cs
+++ c-sharp/CSharpBin/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CsBindgen;
 
-NativeBindings.CallRust(() => Console.WriteLine("Hello, World!"), (val) => Console.WriteLine($"Failed with {val}"));
+NativeBindings.CallRust(() => Console.WriteLine("Hello, World!"), (val) => Console.WriteLine($"Failed with {val}"), (v1, v2) => Console.WriteLine($"v1 = {v1}, v2 = {v2}"));
