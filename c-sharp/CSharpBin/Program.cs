94f15d8d07fb18b64be73415219e50c865851a40
Bennet Bleßmann <bb-github@t-online.de>
2024-06-13 23:36:44 +0200

    adjust formatting

--- c-sharp/CSharpBin/Program.cs
+++ c-sharp/CSharpBin/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CsBindgen;
 
-NativeBindings.CallRust(() => Console.WriteLine("Hello, World!"), (val) => Console.WriteLine($"Failed with {val}"), (v1, v2) => Console.WriteLine($"v1 = {v1}, v2 = {v2}"));
+NativeBindings.CallRust(
+    () => Console.WriteLine("Hello, World!"),
+    (val) => Console.WriteLine($"Failed with {val}"),
+    (v1, v2) => Console.WriteLine($"v1 = {v1}, v2 = {v2}")
+);
